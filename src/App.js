1e7ac8616120f7675f4c57e3c5ee932778d07fc3
JeffRisberg <jrisberg@aol.com>
2019-02-02 19:18:29 -0800

    added NavBar to example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './components/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <NavBar/>
       </div>
     );
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,4 +16,5 @@ const NavBar = () => {
         </div>
     )
 }
+
 export default NavBar;
