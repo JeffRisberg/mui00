27d8f9937deb7c263efcb82796dde5f256d969b4
JeffRisberg <jrisberg@aol.com>
2019-04-18 22:07:44 -0700

    added editorConfig

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import NavBar from './components/NavBar';
 import CourseList from './components/CourseList';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <CourseList />
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="App">
+            <NavBar/>
+            <CourseList/>
+         </div>
+      );
+   }
 }
 
 export default App;
