7830861e99ac85d25edca8041864c768709827fd
JeffRisberg <j.s.risberg@gmail.com>
2024-01-14 16:02:57 -0800

    converted App.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import NavBar from './components/NavBar';
 import CourseList from './components/CourseList';
 import './App.css';
 
-class App extends Component {
-   render() {
-      return (
-         <div className="App">
-            <NavBar/>
-            <CourseList/>
-         </div>
-      );
-   }
-}
+function App() {
+   return (
+      <div className="App">
+         <NavBar/>
+         <CourseList/>
+      </div>
+   )}
 
 export default App;
